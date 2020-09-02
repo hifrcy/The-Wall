08edd8c343d7c043b30ded63dfec9b322857fd1a
hifrcy <hifrcy@gmail.com>
2020-09-02 15:45:45 +0200

    Changes css code

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 
 const Header = () => (
   <header>
-    <img src="images/logo_wall_white.png" id="titleHeader" alt="logo wall" />
+    <img className="ima" src="images/logo_wall_white.png" id="titleHeader" alt="logo wall" />
     <p className="text-center">Transform your tag into an event</p>
   </header>
 );
